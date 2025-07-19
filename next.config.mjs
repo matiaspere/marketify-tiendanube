4b0befb85c84bea65084f71e9a2719664cd739fb
matiaspere <matiperessutti@gmail.com>
2025-07-19 14:56:52 -0300

    fix: allow iframe embedding for Tiendanube

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "ALLOWALL"
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors https://*.tiendanube.com https://*.nuvemshop.com;"
+          }
+        ]
+      }
+    ];
+  }
+};
 
 export default nextConfig;
